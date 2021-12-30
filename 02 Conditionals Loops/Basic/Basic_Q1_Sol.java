6792e055fbf862b46d2ada0a3da99ffaf6a45c92
Soumajit Roy <91539827+Soumajit-Roy@users.noreply.github.com>
2021-12-31 03:17:34 +0530

    adding q1 sol

---
+++ 02 Conditionals Loops/Basic/Basic_Q1_Sol.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Basic_Q1_Sol {
+    public static void main(String[] args) {
+        //Q1. Area Of Circle Java Program
+        System.out.println("Enter Radius of the circle");
+        Scanner a = new Scanner(System.in);
+        int r = a.nextInt();
+        float area = (float) (3.14*(r*r));
+        System.out.println("The area of the circle is : "+area);
+    }
+}
