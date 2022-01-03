7ce17021d497fca63ab8e70bcb9967cfabdd830c
Soumajit Roy <91539827+Soumajit-Roy@users.noreply.github.com>
2022-01-04 02:52:26 +0530

    check

--- 02 Conditionals Loops/Basic/Basic_Q01_Sol.java
+++ 02 Conditionals Loops/Basic/Basic_Q01_Sol.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Basic_Q1_Sol {
+public class Basic_Q01_Sol {
     public static void main(String[] args) {
         //Q1. Area Of Circle Java Program
         System.out.println("Enter Radius of the circle");
