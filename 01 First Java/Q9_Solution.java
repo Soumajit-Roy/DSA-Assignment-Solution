49bab3260c0838ddb479bbcf1d4a31d6df010aea
Soumajit Roy <91539827+Soumajit-Roy@users.noreply.github.com>
2022-09-07 21:55:03 +0530

    Update Q9_Solution.java

--- 01 First Java/Q9_Solution.java
+++ 01 First Java/Q9_Solution.java
@@ -2,20 +2,25 @@ import java.util.Scanner;
 public class Q9_Solution {
     public static void main(String[] args) {
         //Q9. To find Armstrong Number between two given number.
-        System.out.println("Enter a number");
-        Scanner a = new Scanner(System.in);
-        int num = a.nextInt(); //153
-        int num1,temp,total=0;
-        num1=num;
-        while ( num1!=0){
-            temp= num1%10;
-            total= total+ (temp*temp*temp);
-            num1 /= 10;
+        System.out.println("Enter a Number");
+        Scanner x = new Scanner(System.in);
+        int num = x.nextInt();
+        int temp, total=0, digits=0;
+        int numt = num;
+        while (numt>0){
+            numt = numt/10;
+            digits++;;
         }
-        if (num==total){
-            System.out.println("It is an Armstrong Number");
+        numt = num;
+        while (numt!=0){
+            temp = numt%10;
+            total += Math.pow(temp,digits);
+            numt=numt/10;
+        }
+        if (total==num){
+            System.out.println("It is an armstrong number");
         } else {
-            System.out.println("It is not an Armstrong Number");
+            System.out.println("it is not an armstrong number");
         }
     }
 }
